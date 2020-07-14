2b632142c815494d75de6c726ec925919a7e4299
Umair Khan <umair.khan.freelancer7@gmail.com>
2020-07-14 13:54:39 +0500

    implemented simple navigation for menu item /hats/ with react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/HomePage";
 
+const HatsPage = () => <h1>HATS PAGE</h1>;
+
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
+    </Switch>
+  );
 }
 
 export default App;

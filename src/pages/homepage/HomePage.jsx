22e0ce6bd2618d3d30cea1e735014178b18f66c6
Umair Khan <umair.khan.freelancer7@gmail.com>
2020-07-14 12:23:34 +0500

    Broken big homepage component into smaller reusable components and finished styling menu-items

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.scss";
+import Menu from "../../components/menu/Menu";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Menu />
+  </div>
+);
+
+export default HomePage;
